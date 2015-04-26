aedfe1a0a15447f6cc8c1208a4ee294ae96349d1
Till-Julius Krüger <tj.krueger@live.de>
2015-04-26 21:52:07 +0200

    add soundfile

--- mensaagent.sh
+++ mensaagent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-BEEP=/usr/share/sounds/ubuntu/stereo/phone-incoming-call.ogg
+#insert a path for a sound file
+BEEP=/home/tj/Dokumente/mensaagent/nomnom.wav
 
 mealplan=$(curl --silent http://www.studentenwerk-berlin.de/mensen/speiseplan/fu2/01.html)
 
