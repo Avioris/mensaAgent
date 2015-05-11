b01f06307e74b15a48adf451fe762b6e10e76883
Till-Julius Krüger <tj.krueger@live.de>
2015-05-11 11:44:33 +0200

    too

--- mensaagent.sh
+++ mensaagent.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #insert a path for a sound file
 BEEP=/home/tj/Dokumente/mensaagent/nomnom.wav
 
@@ -7,12 +8,13 @@ mealplan=$(curl --silent http://www.studentenwerk-berlin.de/mensen/speiseplan/fu
 #read meals
 while read meal 
 do
-	echo $mealplan | grep "$meal" > /dev/null
+	echo $mealplan | grep -i "$meal" > /dev/null
 	if [ $? = '0' ]; then
 		#message
 		echo "message: MensaAgent\n"$meal| zenity --notification --listen --timeout 1
 		paplay $BEEP
 	fi
+#correct this path too
 done < ~/Dokumente/mensaagent/meals.txt
 
 
